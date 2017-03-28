4cc467dfe4c2e25acc7a47b1ba5ee2e95fe18abe
Rimon Hanna <rimonhanna@users.noreply.github.com>
2017-03-28 21:19:16 +0200

    swift 3.0 support (#11)

--- XCSwiftr/Classes/DataControllers/XCSCommandController.swift
+++ XCSwiftr/Classes/DataControllers/XCSCommandController.swift
@@ -8,97 +8,92 @@
 
 import Foundation
 
-public class XCSCommandController: NSObject {
-    
-    private var scriptName = "objc2swift"
-    private var scriptVersion = "1.0"
-    
-    public func objc2Swift(objcPath: String, completion: ((String) -> Void)) {
-        
-        let bundle = NSBundle(forClass: XCSCommandController.self)
-        
+open class XCSCommandController: NSObject {
+
+    fileprivate var scriptName = "objc2swift"
+    fileprivate var scriptVersion = "1.0"
+
+    open func objc2Swift(_ objcPath: String, completion: @escaping ((String) -> Void)) {
+
+        let bundle = Bundle(for: XCSCommandController.self)
+
         var scriptPath = ""
-        
-        if let path = bundle.pathForResource("\(scriptName)-\(scriptVersion)", ofType: "jar") {
+
+        if let path = bundle.path(forResource: "\(scriptName)-\(scriptVersion)", ofType: "jar") {
             scriptPath = path
         }
-        
+
         let args: [String] = ["-jar", scriptPath, objcPath]
-        
+
         return self.runJavaWithArguments(args, completion: completion)
     }
-    
-    private func runJavaWithArguments(arguments: [String], completion: ((String) -> Void)) {
-        
+
+    fileprivate func runJavaWithArguments(_ arguments: [String], completion: @escaping ((String) -> Void)) {
+
         isJDKInstalled { (available) in
             if available == true {
                 return self.runWithLaunchPath("/usr/bin/java", arguments: arguments, completion: completion)
-            }
-            else {
+            } else {
                 completion("JDK is not available. Please download and install from http://www.oracle.com/technetwork/java/javase/downloads/index.html")
             }
         }
     }
-    
-    private func runBashWithArguments(arguments: [String], completion: ((String) -> Void)) {
+
+    fileprivate func runBashWithArguments(_ arguments: [String], completion: @escaping ((String) -> Void)) {
         return self.runWithLaunchPath("/bin/sh", arguments: arguments, completion: completion)
     }
-    
-    private func runWithLaunchPath(launchPath: String, arguments: [String], completion: ((String) -> Void)) {
 
-        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-        dispatch_async(dispatch_get_global_queue(priority, 0)) {
-            
-            let task = NSTask()
+    fileprivate func runWithLaunchPath(_ launchPath: String, arguments: [String], completion: @escaping ((String) -> Void)) {
+
+        DispatchQueue.global(qos: .default).async {
+
+            let task = Process()
             task.launchPath = launchPath
             task.arguments = arguments
-                        
-            let outputPipe = NSPipe()
-            let errorPipe = NSPipe()
-            
+
+            let outputPipe = Pipe()
+            let errorPipe = Pipe()
+
             task.standardOutput = outputPipe
             task.standardError = errorPipe
-            
+
             let outputFileHandler = outputPipe.fileHandleForReading
             let errorFileHandler = errorPipe.fileHandleForReading
-            
-            task.terminationHandler = { (task: NSTask) in print("termination reason : \(task.terminationReason) | termination status : \(task.terminationStatus)") }
+
+            task.terminationHandler = { (task: Process) in print("termination reason : \(task.terminationReason) | termination status : \(task.terminationStatus)") }
             task.launch()
-            
+
             let outputData = outputFileHandler.readDataToEndOfFile()
             let errorData = errorFileHandler.readDataToEndOfFile()
-            
+
             var result = ""
-            
-            if outputData.length > 0 {
-                result = String(data: outputData, encoding: NSUTF8StringEncoding)!
-            }
-            else if errorData.length > 0 {
-                result = String(data: errorData, encoding: NSUTF8StringEncoding)!
-            }
-            else {
+
+            if outputData.count > 0 {
+                result = String(data: outputData, encoding: String.Encoding.utf8)!
+            } else if errorData.count > 0 {
+                result = String(data: errorData, encoding: String.Encoding.utf8)!
+            } else {
                 result = "Unkown error!"
             }
-            
-            dispatch_async(dispatch_get_main_queue()) {
-                
+
+            DispatchQueue.main.async {
+
                 completion(result)
             }
         }
     }
-    
-    private func isJDKInstalled(completion: ((Bool) -> Void)) {
-        
+
+    fileprivate func isJDKInstalled(_ completion: @escaping ((Bool) -> Void)) {
+
         let args = ["-version"]
-        
+
         self.runBashWithArguments(args) { (result) in
-            
+
             print(result)
-            
-            if result.lowercaseString.rangeOfString("cannot execute binary file") != nil {
+
+            if result.lowercased().range(of: "cannot execute binary file") != nil {
                 completion(false)
-            }
-            else {
+            } else {
                 completion(true)
             }
         }
